dd1e890e2c84ec7888c73ee98be9a8ea37c35dd3
bruzzzla <jah-bru@bruzzzla.de>
2013-02-06 13:38:23 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: bruzzzla
+ * Date: 2/6/13
+ * Time: 12:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+
+
+
+}
